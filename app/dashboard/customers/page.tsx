07e0b82d581ae6dc06b157b875c4331bab9fca24
Suikyo <judeanthony02sayson@gmail.com>
2025-04-28 01:04:54 +0800

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Page() {
+    return (
+        <>
+            <p>Customers Page</p>
+            <div className="h-[200vh] w-full bg-red-200">ehehe</div>
+        </>
+    );
+}
